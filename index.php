25ebb864e575b167073b97b6c6fcb24d7d32279c
sarmatt3 <sarmat.toguzov@inbox.ru>
2026-02-18 11:57:27 +0300

    begin

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A V R O R A</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">Бронирование</a>
+            <a href="">Рестораны</a>
+            <a href="">Контакты</a>
+            <a href="login.php">Вход</a>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>В Х О Д</title>
+</head>
+<body>
+    <button onclick="window.history.back()">Back</button>
+    <form action="" class="login-form">
+        <div class="entry">
+            <label for="">Логин</label>
+            <input type="text" id="login">
+        </div>
+        <div class="entry">
+            <label for="">Пароль</label>
+            <input type="password" id="password">
+        </div>
+        <button type="submit">Войти</button>
+        <a href="">Подключить ресторан</a>
+    </form>
+</body>
+</html>
